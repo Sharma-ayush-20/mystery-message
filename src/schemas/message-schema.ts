eaa7916d2186c9e7d637dec3907dcf5795b49c76
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2026-02-03 22:45:45 +0530

    write Zod package for validation

---
+++ src/schemas/message-schema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+export const messageValidation = z.object({
+    content: z.string()
+    .min(10, {message: 'Content must be at least of 10 characters'})
+    .max(300, {message: 'Content must be not more than 300 characters'}),
+})
